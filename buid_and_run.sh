6077342cd989692ae78de8ba96272ad6e9f7f05c
gallol <luca.gallo@prometeia.com>
2021-08-06 13:42:34 +0200

    Docker compose

---
+++ buid_and_run.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+
+set -e
+
+
+#########################################
+#
+# Download source and build war
+#
+#########################################
+function download_and_build_war {
+	rm -rf ./tmp
+
+	mkdir tmp
+
+	git clone git@github.com:lucamgal/pin-manager.git ./tmp
+
+	cd tmp && chmod +x mvnw && ./mvnw clean package
+
+	cd .. && mv ./tmp/target/*.war ./tomcat/dist/pin-manager.war
+}
+
+
+download_and_build_war
+
+docker-compose down
+
+docker-compose rm -f
+
+docker-compose build --no-cache
+
+docker-compose up -d
